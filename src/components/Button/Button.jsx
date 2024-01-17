e4e3fe3b25184422d8ea7ed2b1528b120922aa52
K.Lysov <K.Lysov@russianpost.ru>
2024-01-17 11:14:54 +0300

    добавление формы

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,7 @@
-import { useState } from 'react';
 import './Button.css';
 
-export const Button = () => {
-	// let text = 'Сохранить';
-	const [text, setText] = useState('Сохранить');
-
-	const clicked = () => {
-		setText(text === 'Сохранить' ? 'Удалить' : 'Сохранить');
-		console.log('Привет');
-	};
-
+export const Button = ({ text, onClick }) => {
 	return (
-		// <button onClick={() => console.log('Привет')} className="button accent">Сохранить</button>
-		<button onClick={clicked} className="button accent">{text}</button>
+		<button className="button accent" onClick={onClick}>{text}</button>
 	);
 };
